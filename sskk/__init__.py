7fd80080d9ba8de1cb56c7737553e04c5768e8e4
Hayaki Saito <user@zuse.jp>
2014-01-04 02:56:06 +0900

    Update version

--- sskk/__init__.py
+++ sskk/__init__.py
@@ -19,7 +19,7 @@
 # ***** END LICENSE BLOCK *****
 
 __author__ = "Hayaki Saito (user@zuse.jp)"
-__version__ = "0.0.118"
+__version__ = "0.0.119"
 __license__ = "GPL v3"
 
 from sskk import main, test_terminal
